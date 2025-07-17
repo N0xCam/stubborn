8a3af6bd57dc2e075b774ac4d88f1b874920d67f
N0xCam <167630075+N0xCam@users.noreply.github.com>
2025-07-17 17:31:28 +0200

    HOME-USER-OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\ProductRepository;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(UserInterface $user): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'user' => $user,
-        ]);
-    }
+    #[Route('/', name: 'app_home')]
+public function index(ProductRepository $productRepository): Response
+{
+    $products = $productRepository->findBy([], ['id' => 'DESC'], 3); // les 3 derniers
+    return $this->render('home/index.html.twig', [
+        'products' => $products,
+    ]);
+}
 }
 
